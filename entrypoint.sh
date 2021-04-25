dd4b8cb896270b3d339f58e83089dfff97a77c75
Marcus Lindblom <m@marcuslindblom.com>
2021-04-25 19:04:26 +0200

    Fix issue with deprecated flag waitForResponse

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-HOST=${3:-"https://yellowlab.tools/api/runs"}
 SCORE=${2:-100}
 URL=$1
 
-GLOBALSCORE=`curl -sS -L -H 'Content-Type: application/json' \
---data '{ 
-"url": "'$URL'",
-"waitForResponse": true
-}' $HOST | jq -r '.scoreProfiles.generic.globalScore'`
+RUNID=$(curl -L -X POST -H 'Content-Type: application/json' \
+--data-raw '{
+	"url": "'$URL'",
+	"waitForResponse": false
+}' $HOST | jq -r '.runId')
+
+check() {
+	STATUS=$(curl -L -X GET 'https://yellowlab.tools/api/runs/'$RUNID'' | jq -r '.status.statusCode')
+}
+
+check #1st execution
+
+#echo $STATUS
+while [ "$STATUS" == "running" ]; do
+	sleep 5
+	check
+done
+
+GLOBALSCORE=$(curl -L -H 'Content-Type: application/json' 'https://yellowlab.tools/api/results/'$RUNID'' | jq -r '.scoreProfiles.generic.globalScore')
 
 echo "::set-output name=score::$GLOBALSCORE"
 
